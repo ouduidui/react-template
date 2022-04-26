bc5db112e401ec3c766dd79994221716a8b71498
Dewey Ou <oushihao97@gmail.com>
2022-04-26 15:01:58 +0800

    feat: mobx presist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,28 @@ import { useStores } from '../stores'
 
 export default observer(() => {
   const { todoListStore } = useStores()
-  const { count, increment } = todoListStore
+  const { todos, addTodo } = todoListStore
+
+  const inputEl = useRef<HTMLInputElement>(null)
+
+  const addTodoHandle = () => {
+    const content = inputEl.current?.value
+    if (content) {
+      addTodo(content)
+      inputEl.current!.value = ''
+    }
+  }
   return (
-    <div onClick={() => increment()}>{count}</div>
+    <div>
+      <div className="text-3xl pb-10">Todo List</div>
+      <input
+        ref={inputEl}
+        placeholder="add a todo"
+        type="text"
+        className="px-4 py-2 w-250px text-center bg-transparent outline-none outline-active:none border border-rounded border-gray-200 border-dark:gray-700"
+        onKeyDown={({ key }) => key === 'Enter' && addTodoHandle()}
+      />
+      <div>{JSON.stringify(todos)}</div>
+    </div>
   )
 })
--- src/stores/todolist.ts
+++ src/stores/todolist.ts
@@ -1,12 +1,37 @@
+import { makePersistable } from 'mobx-persist-store'
+
+interface TodoType {
+  id: number
+  content: string
+  done: boolean
+  createAt: number
+}
+
 class TodoListStore {
-  count = 0
+  todos: TodoType[] = []
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true })
+    makePersistable(this, {
+      name: 'todolist',
+      properties: ['todos'],
+      storage: window.localStorage,
+    })
   }
 
-  increment() {
-    this.count++
+  addTodo(content: string) {
+    this.todos.push({
+      id: this.todos.length + 1,
+      content,
+      done: false,
+      createAt: Date.now(),
+    })
+  }
+
+  todoDone(id: number) {
+    const todo = this.todos.find(todo => todo.id === id)
+    if (todo)
+      todo.done = true
   }
 }
 
