0c1aa3e0727defa60ec635b963467c1134c4abe1
Dewey Ou <oushihao97@gmail.com>
2022-07-16 21:46:33 +0800

    chore: update deps

--- src/hooks/useDark.ts
+++ src/hooks/useDark.ts
@@ -3,6 +3,7 @@ export default function useDark() {
 
   const html = document.getElementsByTagName('html')[0]
   useEffect(() => {
+    html.style.colorScheme = isDark ? 'dark' : 'light'
     if (isDark)
       html.classList.add('dark')
     else
